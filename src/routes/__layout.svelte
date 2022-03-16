1f5b02d82e86b6cac13d6150e4cb8c1c51c29a99
leecharleslaing <71520336+lcharleslaing@users.noreply.github.com>
2022-03-16 13:04:57 -0400

    Added SMUI Top Navbar
    Navbar & Nightmode Toggle

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,5 +1,65 @@
-<script>
+<script lang="ts">
+	import { onMount } from 'svelte';
 	import '../app.css';
+	import './materialUiImports';
+	import type { TopAppBarComponentDev } from '@smui/top-app-bar';
+	import TopAppBar, { Row, Section, Title, AutoAdjust } from '@smui/top-app-bar';
+	import IconButton, { Icon } from '@smui/icon-button';
+	import { mdiMenu, mdiFormatColorFill, mdiWeatherSunny, mdiWeatherNight } from '@mdi/js';
+	import { Svg } from '@smui/common/elements';
+
+	let topAppBar: TopAppBarComponentDev;
+	let darkTheme: boolean | undefined = undefined;
+
+	// onMount(() => {
+	// 	darkTheme = window.matchMedia('(prefers-color-scheme: dark)')
+	// }),
 </script>
 
-<slot />
+<svelte:head>
+	<!-- SMUI Styles -->
+	{#if darkTheme === undefined}
+		<link rel="stylesheet" href="/smui.css" media="(prefers-color-scheme: light)" />
+		<link rel="stylesheet" href="/smui-dark.css" media="screen and (prefers-color-scheme: dark)" />
+	{:else if darkTheme}
+		<link rel="stylesheet" href="/smui.css" />
+		<link rel="stylesheet" href="/smui-dark.css" media="screen" />
+	{:else}
+		<link rel="stylesheet" href="/smui.css" />
+	{/if}
+</svelte:head>
+
+<TopAppBar bind:this={topAppBar} variant="standard">
+	<Row>
+		<Section>
+			<IconButton class="material-icons">menu</IconButton>
+			<Title>IAMBIG2TINY</Title>
+		</Section>
+		<Section align="end" toolbar>
+			<IconButton
+				aria-label="Change Theme"
+				on:click={() => (darkTheme = !darkTheme)}
+				title={darkTheme ? 'Lights On!' : 'Lights Off!'}
+			>
+				<Icon component={Svg} viewBox="0 0 24 24">
+					<path fill="currentColor" d={darkTheme ? mdiWeatherSunny : mdiWeatherNight} />
+				</Icon>
+			</IconButton>
+		</Section>
+	</Row>
+</TopAppBar>
+<AutoAdjust {topAppBar}>
+	<slot />
+</AutoAdjust>
+
+<style>
+	/* Hide everything above this component. */
+	:global(app),
+	:global(body),
+	:global(html) {
+		display: block !important;
+		height: auto !important;
+		width: auto !important;
+		position: static !important;
+	}
+</style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,8 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import Button from '@smui/button';
+</script>
+
+<div class="text-center m-6">
+	<Button color="primary" on:click={() => alert('Whoop Whoop!')}>Click Me!</Button>
+	<Button color="secondary" on:click={() => alert('Whoop Whoop!')}>Click Me!</Button>
+</div>
